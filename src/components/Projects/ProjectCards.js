b018c0ede80c26346593dcdf0dd3ae699d8eda9c
Alex Beamish <beamish.alex@gmail.com>
2023-01-19 19:11:39 -0500

    Add files via upload

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -5,6 +5,7 @@ import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
+  //{props.hasLink && (<a style={{color: '#4A87FF'}} href={props.demoLink}>Learn more</a>)}
   return (
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
@@ -13,26 +14,6 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
-          </Button>
-        )}
       </Card.Body>
     </Card>
   );
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,56 +1,80 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import cinderLogo from "../../Assets/cinderLogo.png";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
   "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
+  const [width, setWidth] = useState(window.innerWidth);
   useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
+    const handleResizeWindow = () => setWidth(window.innerWidth);
+     // subscribe to window resize event "onComponentDidMount"
+     window.addEventListener("resize", handleResizeWindow);
+     return () => {
+       // unsubscribe "onComponentDestroy"
+       window.removeEventListener("resize", handleResizeWindow);
+     };
+   }, []);
   return (
     <div>
       <Container fluid className="resume-section">
+      <Container>
+        <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <Col
+            md={7}
+            style={{
+              justifyContent: "center",
+              paddingTop: "30px",
+              paddingBottom: "50px",
+            }}
+          >
+            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+              The <strong className="purple">CINDER</strong> Project
+            </h1>
+            <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            CINDER (<a style={{color: '#4A87FF'}} href="https://thecinderproject.ca/">thecinderproject.ca</a>) stands for "Connecting Innovators, 
+            Nurturing Demographic Equity and Representation". This project began in 2022
+            when Neila Toofuny, Alec Trepanier and Talia Edwards formed a club at
+            Queen's University. The purpose of the club was to tackle social issues by
+            building software that benefits underrepresented or marginalized communities,
+            with a specific focus on Black, Indigenous and People of Color (BIPOC).
+            <br />
+            <br />
+            Now, The CINDER Project is in the process of being converted from a club at Queen's
+            to a stand-alone project funded by Codempowr. The CINDER Project team,
+            which currently consists of Neila Toofuny, Alex Beamish and Alec Trepanier,
+            is actively working with BIPOC-owned businesses to develop low-cost and highly effective
+            software solutions that meet their needs.
+            <br />
+            <br />
+            If you have a project that aligns with the mission of The CINDER Project,
+            please <a style={{color: '#4A87FF'}} href="https://thecinderproject.ca/#contact">get in touch</a> with our team.
+            You may qualify for our discounted services!
+          </p>
+        </blockquote>
+          </Col>
+          {!(700 < width && width < 1200) && (
+            <Col
+              md={5}
+              style={{ display: "flex", flexDirection: "col", justifyContent: 'center', alignItems: 'center', paddingBottom: "50px", }}
+              className="about-img"
+            >
+              <div className="img-frame2">
+                <img src={cinderLogo} alt="about" className="img-cinder" />
+              </div>
+            </Col>)}
+        </Row>
+      </Container>
         <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-
-        <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
       </Container>
     </div>
   );
